3084a53d4b52a6631262f7eb53bcb52a0598a4fc
Kimiret <juanjobinimelis@gmail.com>
2025-04-03 13:05:10 -0300

    Failed name generalization test in ohce greeting test

--- tests/test_greet.py
+++ tests/test_greet.py
@@ -6,6 +6,11 @@ from datetime import datetime
 
 @patch('main.datetime')
 def test_ohce_greeting(mock_datetime):
-    #Saludo mañana
+
+    #Saludo mañana con nombre Juan
     mock_datetime.now.returnvalue = datetime(2023, 10, 1, 9, 0, 0)
     assert ohce_greeting('Juan') == '¡Buenos días Juan!'
+
+    #Saludo mañana con nombre Pedro
+    mock_datetime.now.returnvalue = datetime(2023, 10, 1, 9, 0, 0)
+    assert ohce_greeting('Pedro') == '¡Buenos días Pedro!'
