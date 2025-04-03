c321f57f2de0e389c51e81178cc56cf65ddcb323
Kimiret <juanjobinimelis@gmail.com>
2025-04-03 13:25:33 -0300

    Correction in datetime mocking and changes to pass morning and afternoon ohce greeting test

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from datetime import datetime
 
+
+# Función que devuelve un saludo basado en la hora actual
 def ohce_greeting(name):
-    # mensaje de saludo
-    return f"¡Buenos días {name}!"
+    hora_actual = datetime.now().hour
+    if hora_actual < 12:
+        return f"¡Buenos días {name}!"
+    else:
+        return f"¡Buenas tardes {name}!"
--- tests/test_greet.py
+++ tests/test_greet.py
@@ -8,25 +8,25 @@ from datetime import datetime
 def test_ohce_greeting(mock_datetime):
 
     #Saludo mañana con nombre Juan
-    mock_datetime.now.returnvalue = datetime(2023, 10, 1, 9, 0, 0)
+    mock_datetime.now.return_value = datetime(2023, 10, 1, 7, 0, 0)
     assert ohce_greeting('Juan') == '¡Buenos días Juan!'
 
     #Saludo mañana con nombre Pedro
-    mock_datetime.now.returnvalue = datetime(2023, 10, 1, 9, 0, 0)
+    mock_datetime.now.return_value = datetime(2023, 10, 1, 9, 0, 0)
     assert ohce_greeting('Pedro') == '¡Buenos días Pedro!'
 
     #Saludo mañana con nombre Luis
-    mock_datetime.now.returnvalue = datetime(2023, 10, 1, 9, 0, 0)
+    mock_datetime.now.return_value = datetime(2023, 10, 1, 11, 0, 0)
     assert ohce_greeting('Luis') == '¡Buenos días Luis!'
 
     #Saludo Tarde con nombre Juan
-    mock_datetime.now.returnvalue = datetime(2023, 10, 1, 15, 0, 0)
+    mock_datetime.now.return_value = datetime(2023, 10, 1, 13, 0, 0)
     assert ohce_greeting('Juan') == '¡Buenas tardes Juan!'
 
     #Saludo Tarde con nombre Pedro
-    mock_datetime.now.returnvalue = datetime(2023, 10, 1, 15, 0, 0)
+    mock_datetime.now.return_value = datetime(2023, 10, 1, 15, 0, 0)
     assert ohce_greeting('Pedro') == '¡Buenas tardes Pedro!'
 
     #Saludo Tarde con nombre Luis
-    mock_datetime.now.returnvalue = datetime(2023, 10, 1, 15, 0, 0)
+    mock_datetime.now.return_value = datetime(2023, 10, 1, 17, 0, 0)
     assert ohce_greeting('Luis') == '¡Buenas tardes Luis!'
