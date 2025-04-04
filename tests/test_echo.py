9e392524b7a33e845667c8fa475ab975c7968cee
Kimiret <juanjobinimelis@gmail.com>
2025-04-04 15:36:13 -0300

    Failed ohce echo generalization test

--- tests/test_echo.py
+++ tests/test_echo.py
@@ -2,4 +2,8 @@ from main import ohce_echo
 import pytest
 
 def test_ohce_echo():
+    # Test de la función ohce_echo
     assert ohce_echo('hola') == 'aloh'
+    assert ohce_echo('mundo') == 'odnum'
+    assert ohce_echo('Python') == 'nohtyP'
+    assert ohce_echo('aibofobia') == 'aibofobia'
