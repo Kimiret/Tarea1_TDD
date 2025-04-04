c5ccc2d19306635002d2a48a0f2604f2a2394971
Kimiret <juanjobinimelis@gmail.com>
2025-04-04 16:20:45 -0300

    Failed ohce palindrome generalization tests

--- tests/test_palindrome.py
+++ tests/test_palindrome.py
@@ -4,3 +4,5 @@ import pytest
 def test_ohce_palindrome():
     # Test de la función ohce_palindrome
     assert ohce_palindrome('aibofobia') == True
+    assert ohce_palindrome('anitalavalatina') == True
+    assert ohce_palindrome('hola mundo') == False
