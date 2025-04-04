abb4ad15e67c300a8caf7f6b57e579c187441f9b
Kimiret <juanjobinimelis@gmail.com>
2025-04-04 16:34:12 -0300

    Failed ohce stop generalization tests

--- tests/test_stop.py
+++ tests/test_stop.py
@@ -3,3 +3,6 @@ import pytest
 
 def test_ohce_stop():
     assert ohce_stop('Stop!') == True
+    assert ohce_stop('stop') == False
+    assert ohce_stop('Exit!') == False
+    assert ohce_stop('') == False
